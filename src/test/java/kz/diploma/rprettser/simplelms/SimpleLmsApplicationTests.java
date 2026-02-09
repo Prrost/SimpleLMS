0e42be443c280d8ffec3621e134c562fe52b2cb2
rprettser <pr.rostislav@icloud.com>
2026-02-09 20:02:33 +0500

    initial. h2 db. liquibase. security for h2

---
+++ src/test/java/kz/diploma/rprettser/simplelms/SimpleLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.diploma.rprettser.simplelms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
